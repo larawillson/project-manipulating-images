3f04246bc447d46cb4bab5aeb047b111625de359
Lara Willson <LaraWillson@Laras-MacBook-Pro.local>
2016-04-12 17:20:35 -0400

    Attempt 4

--- resizeAndAddLogo.py
+++ resizeAndAddLogo.py
@@ -16,32 +16,32 @@ for filename in os.listdir('./originals'):
     
     # skip non-image files and the logo file itself
 
-    im = Image.open(filename)
-    width, height = im.size
+        im = Image.open(filename)
+        width, height = im.size
 
 # Check if image needs to be resized.
 
-    if width > SQUARE_FIT_SIZE and height > SQUARE_FIT_SIZE:
+        if width > SQUARE_FIT_SIZE and height > SQUARE_FIT_SIZE:
         
         # Calculate the new width and height to resize to.
         
-        if width > height:
-            height = int((SQUARE_FIT_SIZE / width) * height)
-            width = SQUARE_FIT_SIZE
-        else:
-            width = int((SQUARE_FIT_SIZE / height) * width)
-            height = SQUARE_FIT_SIZE
+            if width > height:
+                height = int((SQUARE_FIT_SIZE / width) * height)
+                width = SQUARE_FIT_SIZE
+            else:
+                width = int((SQUARE_FIT_SIZE / height) * width)
+                height = SQUARE_FIT_SIZE
 
         # Resize the image.
 
-        print('Resizing %s...' % (filename))
-        im = im.resize((width, height))
+            print('Resizing %s...' % (filename))
+            im = im.resize((width, height))
 
         # Add the logo.
     
-        print('Adding logo to %s...' % (filename))
-        im.paste(logoIm, (width - logoWidth, height - logoHeight), logoIm)
+            print('Adding logo to %s...' % (filename))
+            im.paste(logoIm, (width - logoWidth, height - logoHeight), logoIm)
 
     # Save changes.
 
-        im.save(os.path.join('withLogo', filename))
+            im.save(os.path.join('withLogo', filename))
