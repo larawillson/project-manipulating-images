fb03f53af2f6653d5168d4693833ed5a8223dc1f
Lara Willson <LaraWillson@Laras-MacBook-Pro.local>
2016-04-19 20:19:35 -0400

    Final Attempt

--- resizeAndAddLogo.py
+++ resizeAndAddLogo.py
@@ -8,27 +8,27 @@ logoIm = Image.open( LOGO_FILENAME )
 logoWidth, logoHeight = logoIm.size
 
 os.makedirs('withLogo', exist_ok=True)
-# Loop over all files in the working directory.
-for filename in os.listdir(os.path.join('.','originals')):
-# skip non-image files and the logo file itself 
-    if not (filename.endswith('.png') or filename.endswith('.jpg')) \
-        or filename == LOGO_FILENAME:
-            continue
-    im = Image.open(filename)
+#TODO: Loop over all files in the working directory
+for filename in os.listdir('originals'):
+    if not(filename.endswith('.png') or filename.endswith('.jpg'))\
+       or filename == LOGO_FILENAME:
+        continue 
+    im = Image.open(os.path.join('originals',filename))
     width, height = im.size
-        #Check if image needs to be resized.
+#TODO: Check if file image needs to be resized.
     if width > SQUARE_FIT_SIZE and height > SQUARE_FIT_SIZE:
-# Calculate the new width and height to resize to.
+#TODO: Calculate the new width and height to resize to.
         if width > height:
             height = int((SQUARE_FIT_SIZE / width) * height)
             width = SQUARE_FIT_SIZE
-        else:
-            width = int((SQUARE_FIT_SIZE / height) * width)
-            height = SQUARE_FIT_SIZE
-# Resize the image.
-    print('Resizing %s...' % (filename))
-    im = im.resize((width, height))
-# Add the logo.
-    print ('Adding logo to %s...' % (filename))
-    im.paste(logoIm, (width - logoWidth, height - logoHeight), logoIm)
-    im.save(os.path.join('withLogo', filename))
+    else:
+        width = int((SQUARE_FIT_SIZE / height) * width)
+        height = SQUARE_FIT_SIZE
+#Resize the image.
+        print('Resizing %s...' % (filename))
+        im = im.resize((width, height))
+#TODO: Add the logo.
+        print('Adding logo to %s...' % (filename))
+        im.paste(logoIm, (width - logoWidth, height - logoHeight), logoIm)
+#TODO: Save changes.
+        im.save(os.path.join('withLogo', filename))
